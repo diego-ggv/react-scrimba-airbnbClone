ff4c21eebd7fae0ed7eb7d353852ef698c27d049
Diego Gutierrez <diego_ggv@pm.me>
2023-03-05 23:07:53 -0500

    adjust styling on card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,18 +4,20 @@ import Image1 from '../assets/images/image-1.png'
 function Card() {
   return (
       <div className="section">
-        <div className="card--image">
+        <div className="card">
           <span className="card--availability">SOLD OUT</span>
-          <img src={Image1} alt="swimming lessons"/>
-          
-          <div className="card--rating">
+          <img className="card--image"
+               src={Image1}
+               alt="swimming lessons"/>
+
+          <div className="card--stats">
             <i className="card--star fa-solid fa-star"></i>
-            <p>5.0 <span className="card--rating-lightGray">(6) USA</span></p>
+            <p>5.0 <span className="card--stats-lightGray">(6) • USA</span></p>
           </div>
-          
+
           <p>Life lessons with Katie Zaferes</p>
           <p><strong>From $136</strong> / person</p>
-          
+
         </div>
       </div>
   )
